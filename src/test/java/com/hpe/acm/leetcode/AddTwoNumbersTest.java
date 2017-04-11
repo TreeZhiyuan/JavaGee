2b06a638cbf55802c0a55eaa846a104a83d45b96
Zhi Yuan Cui <zhi-yuan.cui@hpe.com>
2017-04-11 20:39:17 +0800

    reverse digits of an integer consisering the overflow and leading zero situation

--- src/test/java/com/hpe/acm/leetcode/AddTwoNumbersTest.java
+++ src/test/java/com/hpe/acm/leetcode/AddTwoNumbersTest.java
@@ -1,15 +1,15 @@
 package com.hpe.acm.leetcode;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class AddTwoNumbersTest {
+	AddTwoNumbers addTwoNumbers;
 
 	@Before
 	public void setUp() throws Exception {
+		addTwoNumbers = new AddTwoNumbers();
 	}
 
 	@After
@@ -18,14 +18,12 @@ public class AddTwoNumbersTest {
 
 	@Test
 	public void test() {
-		AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
 		addTwoNumbers.addTwoNumbers(addTwoNumbers.initList(new int[] { 2, 4, 3 }),
 				addTwoNumbers.initList(new int[] { 5, 6, 4 }));
 	}
 
 	@Test
 	public void initListTest() {
-		AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
 		ListNode l1 = addTwoNumbers.initList(new int[] { 2, 4, 3 });
 		ListNode l2 = addTwoNumbers.initList(new int[] { 5, 6, 4 });
 	}
---
+++ src/test/java/com/hpe/acm/leetcode/ReverseIntegerTest.java
@@ -0,0 +1,44 @@
+package com.hpe.acm.leetcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReverseIntegerTest {
+	ReverseInteger reverseInteger;
+
+	@Before
+	public void setUp() throws Exception {
+		reverseInteger = new ReverseInteger();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRun() {
+		int x = -1234;
+		long res = 0;
+		while (x != 0) {
+			res = res * 10 + x % 10;
+			x = x / 10;
+		}
+		if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
+			res = 0;
+		}
+	}
+
+	@Test
+	public void test() {
+		assertEquals("", reverseInteger.reverse(123), 321);
+		assertEquals("", reverseInteger.reverse(-123), -321);
+		assertEquals("", reverseInteger.reverse(99599), 99599);
+		assertEquals("", reverseInteger.reverse(-9959), -9599);
+		assertEquals("", reverseInteger.reverse(0), 0);
+		assertEquals("", reverseInteger.reverse(1534236469), 0);
+	}
+
+}
